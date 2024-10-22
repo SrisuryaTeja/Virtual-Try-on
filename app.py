411efd02d8ccb2babe1ea75f78f7e49e89ee5f3b
srisurya <srisurya2002ggs@gmail.com>
2024-10-22 18:05:54 +0530

    final

--- app.py
+++ app.py
@@ -1,44 +1,64 @@
 from flask import Flask,request,jsonify
+from twilio.twiml.messaging_response import MessagingResponse
+from dotenv import load_dotenv
 from twilio.rest import Client
 import requests
 import os
+
+load_dotenv()
 
 app=Flask(__name__)
 
 TWILIO_ACCOUNT_SID=os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN=os.getenv('TWILIO_AUTH_TOKEN')
 TWILIO_WHATSAPP_NUMBER=os.getenv('TWILIO_WHATSAPP_NUMBER')
+TO_NUMBER=os.getenv('TO_NUMBER')
 
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 @app.route('/receive',methods=['POST'])
 
 def receive_images():
-    data=request.get_json()
-    person_image=data['person_image_url']
-    dress_image=data['dress_image_url']
+    try :
+        num_media=int(request.values.get("NumMedia"))
+    except (ValueError,TypeError):
+        return "Invalid request: invalid or missing NumMedia parameter", 400
+    
+    if num_media <2 :
+        response=MessagingResponse()
+        response.message("Please send both the person's image and the dress image.")
+        return str(response)
+    
+
+    person_image=request.values.get('MediaUrl0')
+    dress_image=request.values.get['MediaUrl1']
 
     result=Hfapi(person_image,dress_image)
-    send_response(data['from'],result)
-
-    return jsonify({"status":"Success"})
+    if result:
+        send_response(request.values.get("From"),result)
+        return jsonify({"status":"Success"})
+    else :
+        return jsonify({"status":"Failed to generate try-on result"})
+    
 
 def Hfapi(person_image,dress_image):
     url="https://huggingface.co/spaces/Kwai-Kolors/Kolors-Virtual-Try-On"
     response=requests.post(url,json={'person_image':person_image,
                                      'dress_image':dress_image})
-    return response.json()['result_image']
+    if response.status_code ==200:
+        return response.json().get('result_image')
+    else :
+        return None
 
 def send_response(user_id,image_url):
-
-    if(image_url):
-        message_body=f"Here is the virtual try-on result:{image_url}"
-    else :
-        message_body=f"Try-on failed.Please try again "
-    
-    message=Client.messages.create(
-        body=message_body,
+    message=client.messages.create(
+        # body=message_body,
+        media_url=[image_url],
         from_=TWILIO_WHATSAPP_NUMBER,
-        to=f'Whatsapp:{user_id}'
+        to=user_id
     )
+    print(message.sid)
 
 if __name__=='__main__':
-    app.run(debug=True)
+    image_url="https://th.bing.com/th/id/OIP.tku69jTu8T8LW2ajk0pHWQHaLH?w=202&h=303&c=7&r=0&o=5&dpr=1.5&pid=1.7"
+    send_response(TO_NUMBER,image_url)
+    # app.run(debug=True)
